6f7c22fe26aad7cfcc781d30122897c6e6c43384
karthik-nookala <karthiknookala97@gmail.com>
2018-08-17 12:05:33 +0530

    c2

--- src/main/java/com/nissan/training/Calculator/Clc.java
+++ src/main/java/com/nissan/training/Calculator/Clc.java
@@ -1,17 +1,26 @@
 package com.nissan.training.Calculator;
 
 public class Clc {
+	int res;
 	public int add(int a, int b){
-		return (a+b);
+		res=a+b;
+		System.out.println(res);
+		return (res);
 	}
 	public int sub(int a, int b){
-		return (a-b);
+		res=a-b;
+		System.out.println(res);
+		return (res);
 	}
 	public int mult(int a, int b){
-		return (a*b);
+		res=a*b;
+		System.out.println(res);
+		return (res);
 	}
 	public int divi(int a, int b){
-		return (a/b);
+		res=a/b;
+		System.out.println(res);
+		return (res);
 	}
 
 }
